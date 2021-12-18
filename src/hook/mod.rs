40b67a78cd1b9fe53803aac8c71b76b44afc84f0
Ao Chen <chenao3220@gmail.com>
2021-12-18 15:42:40 +0800

    [signature] Move verify_signature to hook

--- src/hook/mod.rs
+++ src/hook/mod.rs
@@ -1,3 +1,15 @@
 pub mod dto;
 
 pub use dto::*;
+
+use hmac::{Hmac, Mac, NewMac};
+use sha2::Sha256;
+
+pub fn verify_signature(secret_key: &str, payload: &[u8], signature: &str) -> bool {
+    let mut mac = Hmac::<Sha256>::new_from_slice(secret_key.as_bytes())
+        .expect("HMAC can take key of any size");
+    mac.update(payload);
+    let payload_sig = base64::encode(mac.finalize().into_bytes());
+
+    payload_sig == signature
+}
--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,6 @@ pub mod auth;
 pub mod builder;
 pub mod client;
 pub mod config;
-pub mod encrypt;
 pub mod error;
 pub mod hook;
 pub mod pagination;
