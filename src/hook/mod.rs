0b8cf60bde18394cdbb869f4e199dee139d5d702
Ao Chen <chenao3220@gmail.com>
2021-12-18 16:58:47 +0800

    [hook] Fix hook::verify_signature

--- src/hook/mod.rs
+++ src/hook/mod.rs
@@ -9,7 +9,7 @@ pub fn verify_signature(secret_key: &str, payload: &[u8], signature: &str) -> bo
     let mut mac = Hmac::<Sha256>::new_from_slice(secret_key.as_bytes())
         .expect("HMAC can take key of any size");
     mac.update(payload);
-    let payload_sig = base64::encode(mac.finalize().into_bytes());
+    let payload_sig = format!("{:#x}", mac.finalize().into_bytes());
 
     payload_sig == signature
 }
